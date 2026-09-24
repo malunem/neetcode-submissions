2cd65e2ca6a2ef4cc54d76b3913364fc2696d3cf
Marcella Malune <63505124+malunem@users.noreply.github.com>
2026-09-24 23:25:13 +0100

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures: number[]): number[] {
+        let results: number[] = new Array(temperatures.length).fill(0);
+
+        const stack: number[] = [];
+
+        for (let i = 0; i < temperatures.length; i++) {
+            let last = stack.pop();
+
+            if (last === undefined) {
+                stack.push(i);
+                continue;
+            }
+
+            while (last !== undefined && temperatures[last] < temperatures[i]) {
+                results[last] = i - last;
+                last = stack.pop();
+            }
+
+            stack.push(last);
+            stack.push(i);
+        }
+
+        return results;
+    }
+}
