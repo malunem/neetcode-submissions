395b22437fd5a1dd395059f73610303941e54419
Marcella Malune <63505124+malunem@users.noreply.github.com>
2026-09-24 18:08:36 +0100

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        let result = 0;
+        let seen: Map<string, number> = new Map();
+        let startI = 0;
+        for (let endI = 0; endI < s.length; endI++) {
+            const newChar = s[endI];
+            if (!seen.has(newChar) || seen.get(newChar) < startI) {
+                seen.set(newChar, endI);
+                const newLength = endI-startI+1;
+                if (newLength > result) result = newLength;
+            } else {
+                startI = seen.get(newChar)+1;
+                seen.set(newChar,endI)
+            }
+        }
+
+        return result;
+    }
+}
